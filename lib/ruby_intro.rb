9c4ca4f1753249bf7dd5d9bb3da28f49fddc0d51
EC2 Default User <ec2-user@ip-172-31-44-204.us-east-2.compute.internal>
2019-02-05 06:29:03 +0000

    CS606 homework 1 finished

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,108 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  sum = 0;
+  i = 0;
+  until i == arr.length do
+    sum += arr[i]
+    i = i + 1
+  end
+  return sum
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  sum = 0
+  if arr.length == 0
+    sum = 0
+  elsif arr.length == 1
+    sum = arr[0]
+  else
+    max_arr = arr.max(2)
+    sum = max_arr[0] + max_arr[1]
+  end
+  return sum
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  if arr.length < 2
+    return false
+  end
+  arr = arr.sort
+  i = 0
+  j = arr.length - 1
+  until i == j do
+    if arr[i] + arr[j] > n
+      j = j - 1
+    elsif arr[i] + arr[j] < n
+      i = i + 1
+    else
+      return true
+    end
+  end
+  return false
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  str = "Hello, #{name}"
+  return str;
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  if s =~ /^[a-zA-Z]/
+    if s =~ /^[aeiou]/i
+      return false
+    else
+      return true
+    end
+  else
+    return false
+  end
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  if s.size == 0 || s.count('01') != s.size
+    return false
+  elsif s.to_i == 0
+    return true
+  elsif s =~ /00$/
+    return true
+  else
+    return false
+  end
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  
+  def initialize(isbn_number, money)
+    if (isbn_number.size == 0 || money <= 0)
+      raise ArgumentError, "Argument Error Message"
+    end
+    @isbn = isbn_number
+    @price = money
+  end
+  
+  def isbn
+    @isbn
+  end
+    
+  def isbn=(isbn_number)
+    @isbn = isbn_number
+  end
+  
+  def price
+    @price
+  end
+  
+  def price=(money)
+    @price = money
+  end
+  
+  def price_as_string
+    str = "$#{'%.2f' % price}"
+    return str
+  end
 end
